93fe29128eb2c0b9556d8c3d70d4d4e8e59addc2
matthewpeak <matthewpea@gmail.com>
2020-02-24 17:17:06 -0500

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,20 @@
+class Artist
+    attr_accessor :name 
+    def initialize(name)
+        @name=name
+    end
+   
+    def songs 
+        Song.all{|elem| elem.artist.name = @name}
+    end
+    def add_song(song)
+       song.artist = self 
+    end 
+    def add_song_by_name(name)
+       new_song = Song.new(name)
+       new_song.artist = self 
+    end 
+    def self.song_count
+       Song.all.length
+    end
+end
---
+++ lib/post.rb
@@ -0,0 +1,15 @@
+class Post
+    attr_accessor :title, :author
+    @@all=[]
+    def initialize(title)
+        @title=title
+        @author = author
+        Post.all << self
+    end
+    def self.all
+        @@all
+    end
+    def author_name
+        author.name if author
+    end
+end
